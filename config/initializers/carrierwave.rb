d0e1ae71f26bef7d310a13d935c96e7267b5d993
gatescchi <t.ozawa0723@gmail.com>
2020-08-28 20:58:30 +0900

    complete

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,8 @@ CarrierWave.configure do |config|
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'aqualists'                # required
-  config.fog_public     = true                                                 # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+  config.fog_directory  = 'aquarists'                # required
+  #config.fog_public     = true                                                 # optional, defaults to true
+  #config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
